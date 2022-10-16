f3b6ffddbb6d58abc3b1ba84e1de12cd13ab6b44
ixtf <ixtf1984@gmail.com>
2022-10-16 16:07:50 +0800

    init per-commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,10 @@
 pluginManagement {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    plugins {
-        id("com.diffplug.spotless") version "6.11.0"
-    }
+  repositories {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+  }
+  plugins { id("com.diffplug.spotless") version "6.11.0" }
 }
-rootProject.name = "core"
 
+rootProject.name = "core"
--- src/main/kotlin/Jcodec.kt
+++ src/main/kotlin/Jcodec.kt
@@ -11,58 +11,58 @@ import javax.crypto.SecretKeyFactory
 import javax.crypto.spec.PBEKeySpec
 
 object Jcodec {
-    @JvmStatic
-    fun password(): String {
-        J.isEmpty(null)
-        return password("123456")
-    }
+  @JvmStatic
+  fun password(): String {
+    J.isEmpty(null)
+    return password("123456")
+  }
 
-    @JvmStatic
-    fun password(password: String): String {
-        val salt = ByteArray(16)
-        SecureRandom.getInstance("SHA1PRNG").nextBytes(salt)
-        val iterationCount = 30000
-        val keySize = 160
-        val hash = password(password, salt, iterationCount, keySize)
-        val baos = ByteArrayOutputStream()
-        baos.write(255)
-        val oos = ObjectOutputStream(baos)
-        oos.write(hash.size)
-        oos.write(salt.size)
-        oos.write(hash)
-        oos.write(salt)
-        oos.writeInt(iterationCount)
-        oos.writeInt(keySize)
-        oos.flush()
-        return Base64.encode(baos.toByteArray())
-    }
+  @JvmStatic
+  fun password(password: String): String {
+    val salt = ByteArray(16)
+    SecureRandom.getInstance("SHA1PRNG").nextBytes(salt)
+    val iterationCount = 30000
+    val keySize = 160
+    val hash = password(password, salt, iterationCount, keySize)
+    val baos = ByteArrayOutputStream()
+    baos.write(255)
+    val oos = ObjectOutputStream(baos)
+    oos.write(hash.size)
+    oos.write(salt.size)
+    oos.write(hash)
+    oos.write(salt)
+    oos.writeInt(iterationCount)
+    oos.writeInt(keySize)
+    oos.flush()
+    return Base64.encode(baos.toByteArray())
+  }
 
-    @JvmStatic
-    fun checkPassword(encryptPassword: String, password: String): Boolean {
-        val bais = ByteArrayInputStream(Base64.decode(encryptPassword))
-//        val version = bais.read()
-        if (bais.read() != 255) {
-            return false
-        }
-        val ois = ObjectInputStream(bais)
-        val hash = ByteArray(ois.read())
-        val salt = ByteArray(ois.read())
-        ois.read(hash)
-        ois.read(salt)
-        val iterationCount = ois.readInt()
-        val keySize = ois.readInt()
-        val _hash = password(password, salt, iterationCount, keySize)
-        return Arrays.equals(hash, _hash)
+  @JvmStatic
+  fun checkPassword(encryptPassword: String, password: String): Boolean {
+    val bais = ByteArrayInputStream(Base64.decode(encryptPassword))
+    //        val version = bais.read()
+    if (bais.read() != 255) {
+      return false
     }
+    val ois = ObjectInputStream(bais)
+    val hash = ByteArray(ois.read())
+    val salt = ByteArray(ois.read())
+    ois.read(hash)
+    ois.read(salt)
+    val iterationCount = ois.readInt()
+    val keySize = ois.readInt()
+    val _hash = password(password, salt, iterationCount, keySize)
+    return Arrays.equals(hash, _hash)
+  }
 
-    private fun password(
-        password: String,
-        salt: ByteArray,
-        iterationCount: Int,
-        keySize: Int
-    ): ByteArray {
-        val spec = PBEKeySpec(password.toCharArray(), salt, iterationCount, keySize)
-        val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
-        return factory.generateSecret(spec).encoded
-    }
+  private fun password(
+      password: String,
+      salt: ByteArray,
+      iterationCount: Int,
+      keySize: Int
+  ): ByteArray {
+    val spec = PBEKeySpec(password.toCharArray(), salt, iterationCount, keySize)
+    val factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+    return factory.generateSecret(spec).encoded
+  }
 }
